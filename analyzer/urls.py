61db3f88d8ee034ef1bda6ad5296066a7a143d8b
Ryo--code <ryotanakamura0310@gmail.com>
2024-12-16 22:53:50 +0900

    text output

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views #views.pyから関数をインポート
 
 urlpatterns = [
-    path('',views.upload, name="upload"), # ルートURLをviews.uploadに紐づける
+    path('',views.upload, name="upload"), # ルートURLをviews.uploadに紐づける.初期画面なので,urlパターンは空でOK
+    path('result/',views.result, name='result'), #views.resultに紐づける
 ]
--- analyzer/views.py
+++ analyzer/views.py
@@ -4,3 +4,12 @@ from django.views.generic import TemplateView
 def upload(request):
     #http://127.0.0.1:8000/で表示されるページ
     return render(request, 'analyzer/upload.html')
+
+def result(request):
+    if request.method == 'POST': #フォーム送信時のHTTPリクエストがPOSTメソッドである場合、条件が True 
+        input_text = request.POST.get('name') #フォームからデータを取得 .get()メソッドで特定のキーを取得
+        # ここで解析処理を実装（仮に入力データをそのまま返す）
+        result_data = f"入力された文章:{input_text}"
+        return render(request, 'analyzer/result.html',{'result': result_data})
+    else:
+        return render(request, 'analyzer/result.html',{'result':"データが送信されてません。"}) #第三引数はcontextで、キーと値で構成される辞書
