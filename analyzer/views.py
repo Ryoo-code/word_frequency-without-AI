5d6b2a63af556a021bb12c392150112233e08ba3
Ryo--code <ryotanakamura0310@gmail.com>
2024-12-17 16:48:51 +0900

    Add stop words

--- analyzer/views.py
+++ analyzer/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from collections import Counter #Python標準ライブラリの Counter を使って、リスト内の要素（単語）の頻度を簡単にカウン
+import spacy
+
+#英語モデルをダウンロード
+nlp = spacy.load("en_core_web_sm")
 
 def upload(request):
     # http://127.0.0.1:8000/ で表示されるページ
@@ -9,10 +13,15 @@ def upload(request):
 def result(request):
     if request.method == 'POST':  # POSTリクエストの場合
         input_text = request.POST.get('name', '').strip()  # フォームデータを取得、空白を除去     
-        # --- 単語の分割と頻度カウント ---
-        words = input_text.split()  # テキストをスペースで分割
-        word_count = dict(Counter(words)) # 単語の頻度をカウント,result.htmlの.items()は辞書型のみ有効
-        
+        # --- spacyを使ってストップワードを除外 ---
+        doc = nlp(input_text) #入力されたテキストをトークナイズして、基本形を取得したり、ストップワードの判断を行う
+        # 文字列を取得し、ストップワードとアルファベットのみで構成されてない単語を除去
+        filtered_words = [token.text for token in doc 
+                          if not token.is_stop and token.is_alpha and not token.is_punct] #token.is_punctは記号をTrue
+        word_count = dict(Counter(filtered_words))
+        #word_countという辞書から特定のキー"items"を取り除く
+        word_count = {key: value for key, value in word_count.items() if key != "items"}
+
         # 結果をテンプレートに渡す
         return render(request, 'analyzer/result.html', {
             'input_text': input_text,
